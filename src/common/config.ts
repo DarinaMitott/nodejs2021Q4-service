3a4e2100bb72f4782b5656a45672631dafb5d978
Darina <darinahohlova06@gmail.com>
2021-12-25 03:02:48 +0300

    feat: added `LOG_LEVEL` and `EXIT_ON_UNHANDLED_EXCEPTION` options to .env

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,8 +5,8 @@ config({
   path: join(__dirname, '../../.env')
 });
 
-export const {PORT} = process.env;
-export const {NODE_ENV} = process.env;
-export const {MONGO_CONNECTION_STRING} = process.env;
+export const {PORT, NODE_ENV, MONGO_CONNECTION_STRING} = process.env;
 export const {JWT_SECRET_KEY} = process.env;
 export const AUTH_MODE = process.env.AUTH_MODE === 'true';
+export const {LOG_LEVEL} = process.env;
+export const EXIT_ON_UNHANDLED_EXCEPTION = process.env.EXIT_ON_UNHANDLED_EXCEPTION === 'true';
