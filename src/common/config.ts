1a33ffff65f020727a9ccc364f1c936384cf28b5
Darina <darinahohlova06@gmail.com>
2022-01-23 15:56:15 +0300

    feat: config prepared for auth feature

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,8 +6,9 @@ config({
 });
 
 export const {PORT, NODE_ENV} = process.env;
-export const {JWT_SECRET_KEY} = process.env;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'default-secret';
 export const AUTH_MODE = process.env.AUTH_MODE === 'true';
 export const {LOG_LEVEL} = process.env;
 export const EXIT_ON_UNHANDLED_EXCEPTION = process.env.EXIT_ON_UNHANDLED_EXCEPTION === 'true';
 export const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT } = process.env;
+export const HASH_ROUNDS = Number(process.env.HASH_ROUNDS) ?? 10;
